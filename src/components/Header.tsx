f6630d2576ab2a773b9f239e21a7f321a700d119
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-03 16:06:10 +0200

    alternates dark mode button color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,38 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar, Typography, makeStyles } from '@material-ui/core';
 import { Brightness7, Brightness4 } from '@material-ui/icons';
 import { Dispatch } from 'react';
 import { useThemeDispatch, useThemeState } from './context/theme/context';
 import { Action } from './context/theme/reducer';
 
+const useStyles = makeStyles({
+    headerLinkStyles: {
+        flex: 1,
+        textDecoration: 'none',
+        // color: '#545454',
+    },
+    iconLight: {
+        color: '#d6d6d6',
+    },
+    iconDark: {
+        color: '#9d9d9d',
+    },
+});
+
 const Header = () => {
     const { currentTheme } = useThemeState();
     const dispatchTheme: Dispatch<Action> = useThemeDispatch();
+    const classes = useStyles();
 
     return (
         <AppBar position="static" square>
             <Toolbar>
                 <Typography variant="h5">The Travel Almanac Style Guide</Typography>
-                <div onClick={() => dispatchTheme({ type: 'TOGGLE_THEME' })}>
+                <IconButton
+                    className={currentTheme === 'dark' ? classes.iconLight : classes.iconDark}
+                    onClick={() => dispatchTheme({ type: 'TOGGLE_THEME' })}
+                >
                     {currentTheme === 'dark' ? <Brightness7 /> : <Brightness4 />}
-                </div>
+                </IconButton>
             </Toolbar>
         </AppBar>
     );
