ff5b228eb6587b0bb9c58349ef7be4adfaccf86c
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-14 09:40:34 +0200

    imports minion pro font

--- src/components/styles/theme.ts
+++ src/components/styles/theme.ts
@@ -2,6 +2,27 @@ import { ThemeOptions } from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
 
 export const themeLight: ThemeOptions = {
+    typography: {
+        fontFamily: 'Minion, Arial',
+    },
+    overrides: {
+        MuiCssBaseline: {
+            '@global': {
+                '@font-face': [
+                    {
+                        fontFamily: 'Minion',
+                        fontStyle: 'normal',
+                        fontDisplay: 'swap',
+                        fontWeight: 400,
+                        src: `
+                            local('Minion'),
+                            url('/minion-pro-condensed.ttf') format('truetype')
+                        `,
+                    },
+                ],
+            },
+        },
+    },
     palette: {
         primary: {
             main: '#556cd6',
