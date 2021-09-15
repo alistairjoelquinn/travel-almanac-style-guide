097cb946fb4e50a87c60bd7cdea6bcbbaa7d08c1
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-15 16:20:44 +0200

    nav bar complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ const NavStyles = styled.nav`
             outline: none;
             border: none;
             cursor: pointer;
-            height: 1.5rem;
+            height: 1.8rem;
             background: linear-gradient(currentColor 0 0) bottom / var(--d, 0) 2px no-repeat;
             transition: 0.5s;
             &.underline {
