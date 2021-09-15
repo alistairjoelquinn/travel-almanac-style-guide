af82ef9be3d65a865652ec4b45bdfb108343a9eb
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-15 11:54:11 +0200

    nav item underlines when user visits page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,17 +27,21 @@ const NavStyles = styled.nav`
     justify-content: space-around;
     border-top: 2px solid black;
     border-bottom: 2px solid black;
-    span {
+    div {
         width: 100%;
-        line-height: 2rem;
-        text-align: center;
+        height: 2.5rem;
+        margin-top: 0.3rem;
         font-family: MinionBold;
         color: black;
-        padding-top: 0.3rem;
-        text-decoration: none;
-        cursor: pointer;
-        .underline {
-            border-bottom: 1px solid black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        span {
+            cursor: pointer;
+            height: 1.5rem;
+            &.underline {
+                border-bottom: 2px solid black;
+            }
         }
     }
 `;
@@ -53,21 +57,31 @@ const Header = () => {
                 <h5>THE TRAVEL ALMANAC STYLE GUIDE</h5>
             </HeaderStyles>
             <NavStyles className="page-nav">
-                <Link href="/" passHref>
-                    <span className="underline">Common Words</span>
-                </Link>
-                <Link href="/punctuation" passHref>
-                    <span>Punctuation</span>
-                </Link>
-                <Link href="/dates-numbers" passHref>
-                    <span>Dates & Numbers</span>
-                </Link>
-                <Link href="/best-practise" passHref>
-                    <span>Best Practise</span>
-                </Link>
-                <Link href="/tone-of-voice" passHref>
-                    <span>Tone of Voice</span>
-                </Link>
+                <div>
+                    <Link href="/" passHref>
+                        <span className="underline">Common Words</span>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/punctuation" passHref>
+                        <span>Punctuation</span>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/dates-numbers" passHref>
+                        <span>Dates & Numbers</span>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/best-practise" passHref>
+                        <span>Best Practise</span>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/tone-of-voice" passHref>
+                        <span>Tone of Voice</span>
+                    </Link>
+                </div>
             </NavStyles>
         </>
     );
