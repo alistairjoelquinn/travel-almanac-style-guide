c64f01d030b513216b4b90e7086fc67499a8715f
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-16 10:48:26 +0200

    basic card structure and page layout added to punctuation page' ' gp ' ' '

--- src/components/styles/Typography.ts
+++ src/components/styles/Typography.ts
@@ -4,7 +4,7 @@ import normalize from 'styled-normalize';
 const Typography = createGlobalStyle`
     ${normalize}
     html {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-family: Minion, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size: 10px;
     }
     body {
@@ -20,6 +20,12 @@ const Typography = createGlobalStyle`
     @font-face {
         font-family: 'MinionBold';
         src: url('/minion-pro-bold.ttf') format('truetype');
+        font-weight: normal;
+        font-display: swap;
+    }
+    @font-face {
+        font-family: 'Minion';
+        src: url('/minion-pro-condensed.ttf') format('truetype');
         font-weight: normal;
         font-display: swap;
     }
--- src/pages/punctuation.tsx
+++ src/pages/punctuation.tsx
@@ -1,7 +1,56 @@
-const punctuation = () => {
+import styled from 'styled-components';
+import punctuation from '../../content/punctuation.json';
+
+const PageStyles = styled.div`
+    padding: 2rem;
+`;
+
+const GridStyles = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+`;
+
+const GridItemStyles = styled.dl`
+    border: 1px solid black;
+    border-radius: 1rem;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+    margin: 4rem;
+    margin-top: 2rem;
+    &:hover {
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    }
+    dt {
+        align-self: flex-start;
+        margin-bottom: 1rem;
+        font-weight: bold;
+    }
+    dd {
+        align-self: flex-end;
+        text-align: left;
+    }
+`;
+
+const Punctuation = () => {
     console.log('punc');
 
-    return <div>Punctuation page</div>;
+    return (
+        <PageStyles>
+            <GridStyles>
+                {punctuation.map((item) => (
+                    <GridItemStyles key={item.title}>
+                        <dt>{item.title}</dt>
+                        <dd>{item.value}</dd>
+                    </GridItemStyles>
+                ))}
+            </GridStyles>
+        </PageStyles>
+    );
 };
 
-export default punctuation;
+export default Punctuation;
