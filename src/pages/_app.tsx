eea9e41996145b02242daeef61b9040526731ea2
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-16 14:37:16 +0200

    adds react spring for handlinig page animaitons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,39 @@
 import type { AppProps } from 'next/app';
+import { useTransition, animated } from 'react-spring';
 
 import GlobalStyles from '@/components/styles/GlobalStyles';
 import Typography from '@/components/styles/Typography';
 import Page from '@/components/Page';
 
-const App = ({ Component, pageProps }: AppProps) => (
-    <>
-        <GlobalStyles />
-        <Typography />
-        <Page>
-            <Component {...pageProps} />
-        </Page>
-    </>
-);
+const App = (props: AppProps) => {
+    const transitionItems = [
+        {
+            id: props.router.route,
+            Component: props.Component,
+            pageProps: props.pageProps,
+        },
+    ];
+
+    const transition = useTransition(transitionItems, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        keys: (item) => item.id,
+    });
+
+    return (
+        <>
+            <GlobalStyles />
+            <Typography />
+            <Page>
+                {transition((styles, { Component, pageProps }) => (
+                    <animated.div style={styles}>
+                        <Component {...pageProps} />
+                    </animated.div>
+                ))}
+            </Page>
+        </>
+    );
+};
 
 export default App;
