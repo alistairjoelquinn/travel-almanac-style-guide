5ba65a2a2679fbbd7299d2e65085528ffe8f6528
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-16 11:09:12 +0200

    updates best practise json data to reflect new page structure

--- src/pages/best-practise.tsx
+++ src/pages/best-practise.tsx
@@ -1,7 +1,17 @@
-const BestPractise = () => {
-    console.log('best practise');
+import { GridItemStyles, GridStyles, PageStyles } from '@/components/styles/CardPageStyles';
+import bestPractise from '../../content/best-practise.json';
 
-    return <div>Best Practise page</div>;
-};
+const BestPractise = () => (
+    <PageStyles>
+        <GridStyles>
+            {bestPractise.map((item) => (
+                <GridItemStyles key={item.title}>
+                    <dt>{item.title}</dt>
+                    <dd>{item.value}</dd>
+                </GridItemStyles>
+            ))}
+        </GridStyles>
+    </PageStyles>
+);
 
 export default BestPractise;
