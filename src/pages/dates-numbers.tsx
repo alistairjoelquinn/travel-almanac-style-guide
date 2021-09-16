44f386b9e2605b4ea3d4a89b2304e0dc3c1dc717
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-16 11:04:33 +0200

    adds dates and numbers page

--- src/pages/dates-numbers.tsx
+++ src/pages/dates-numbers.tsx
@@ -1,7 +1,21 @@
-const DatesNumbers = () => {
-    console.log('dates & numbers');
+import { GridItemStyles, GridStyles, PageStyles } from '@/components/styles/CardPageStyles';
+import datesNumbers from '../../content/dates-numbers.json';
 
-    return <div>Dates Numbers page</div>;
-};
+const DatesNumbers = () => (
+    <PageStyles>
+        <GridStyles>
+            {datesNumbers.map((item) => (
+                <GridItemStyles key={item.title}>
+                    <dt>{item.title}</dt>
+                    <ul>
+                        {item.value.map((val) => (
+                            <li key={val}>{val}</li>
+                        ))}
+                    </ul>
+                </GridItemStyles>
+            ))}
+        </GridStyles>
+    </PageStyles>
+);
 
 export default DatesNumbers;
