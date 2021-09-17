aec38c82ff05569f42b55174880a4c055658132b
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-17 18:17:30 +0200

    adds transition spring for best practise page

--- src/pages/best-practise.tsx
+++ src/pages/best-practise.tsx
@@ -1,23 +1,34 @@
 import { GridItemStyles, GridStyles, PageStyles } from '@/components/styles/CardPageStyles';
+import { useTransition } from 'react-spring';
 import bestPractise from '../../content/best-practise.json';
 
-const BestPractise = () => (
-    <PageStyles>
-        <GridStyles page="best-practise">
-            {bestPractise.map((item) => (
-                <GridItemStyles key={item.title}>
-                    <dt>{item.title}</dt>
-                    <div>
-                        {item.value.map((val) => (
-                            <dd key={val} className="paragraphs">
-                                {val}
-                            </dd>
-                        ))}
-                    </div>
-                </GridItemStyles>
-            ))}
-        </GridStyles>
-    </PageStyles>
-);
+const BestPractise = () => {
+    const transition = useTransition(bestPractise, {
+        from: { opacity: 0, transform: 'translate3d(0,4rem,0)' },
+        enter: { opacity: 1, transform: 'translate3d(0,0,0)' },
+        leave: { opacity: 0, transform: 'translate3d(0,-4rem,0)' },
+        trail: 100,
+        config: { mass: 1, tension: 200, friction: 10, clamp: true },
+    });
+
+    return (
+        <PageStyles>
+            <GridStyles page="best-practise">
+                {transition((animation, item) => (
+                    <GridItemStyles style={animation} key={item.title}>
+                        <dt>{item.title}</dt>
+                        <div>
+                            {item.value.map((val) => (
+                                <dd key={val} className="paragraphs">
+                                    {val}
+                                </dd>
+                            ))}
+                        </div>
+                    </GridItemStyles>
+                ))}
+            </GridStyles>
+        </PageStyles>
+    );
+};
 
 export default BestPractise;
--- src/pages/punctuation.tsx
+++ src/pages/punctuation.tsx
@@ -1,5 +1,5 @@
 import { GridItemStyles, GridStyles, PageStyles } from '@/components/styles/CardPageStyles';
-import { useTransition, config } from 'react-spring'
+import { useTransition } from 'react-spring';
 
 import punctuation from '../../content/punctuation.json';
 
@@ -14,19 +14,21 @@ const Punctuation = () => {
         enter: { opacity: 1, transform: 'translate3d(0,0,0)' },
         leave: { opacity: 0, transform: 'translate3d(0,-4rem,0)' },
         trail: 100,
-        config: {mass:1, tension:200, friction:10, clamp: true},
+        config: { mass: 1, tension: 200, friction: 10, clamp: true },
     });
 
-    return <PageStyles>
-        <GridStyles page="punctuation">
-            {transition((animation, item) => (
-                <GridItemStyles style={animation} key={item.title}>
-                    <dt>{item.title}</dt>
-                    <dd>{item.value}</dd>
-                </GridItemStyles>
-            ))}
-        </GridStyles>
-    </PageStyles>
+    return (
+        <PageStyles>
+            <GridStyles page="punctuation">
+                {transition((animation, item) => (
+                    <GridItemStyles style={animation} key={item.title}>
+                        <dt>{item.title}</dt>
+                        <dd>{item.value}</dd>
+                    </GridItemStyles>
+                ))}
+            </GridStyles>
+        </PageStyles>
+    );
 };
 
 export default Punctuation;
