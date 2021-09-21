3913627eb7c2305f2223dbdf108bc7ee81eb678c
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-09-21 15:28:13 +0200

    common words cards appear on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,31 @@
+import { useTransition } from 'react-spring';
 import Head from 'next/head';
 
-const Home = () => (
-    <div>
-        <Head>
-            <title>Travel Almanac Style Guide</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+import { GridItemStyles, GridStyles, PageStyles } from '@/components/styles/CardPageStyles';
+import commonWords from '../../content/common-words.json';
+import { springValues } from './punctuation';
 
-        <main>Common Words</main>
-    </div>
-);
+const Home = () => {
+    const transition = useTransition(commonWords, springValues);
+
+    return (
+        <>
+            <Head>
+                <title>Travel Almanac Style Guide</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <PageStyles>
+                <GridStyles page="common-words">
+                    {transition((animation, item) => (
+                        <GridItemStyles style={animation} key={item.title}>
+                            <dt>{item.title}</dt>
+                            <dd>{item.value}</dd>
+                        </GridItemStyles>
+                    ))}
+                </GridStyles>
+            </PageStyles>
+        </>
+    );
+};
 
 export default Home;
