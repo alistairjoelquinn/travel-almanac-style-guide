595a9d4797ba8d9962eed89b3af3af7cf91233fc
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-11-29 08:26:59 +0100

    updates cms schema to include tone of voice article order

--- server/schemas/tone-of-voice.js
+++ server/schemas/tone-of-voice.js
@@ -18,5 +18,11 @@ export default {
             type: 'text',
             description: 'This HTML will be shown exactly on screen! Be sure to always use the correct tags. If you are unsure then check the following resource https://developer.mozilla.org/en-US/docs/Web/HTML/Element'
         },
+        {
+            name: 'order',
+            title: 'Article Order',
+            type: 'number',
+            description: 'This value should be an integer. A lower number will always be listed before a higher number i.e. article 3 will appear before article 4.'
+        }
     ]
 };
