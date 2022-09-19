e242e53d426448c1cf3561641f23a8969dc3e84f
alistairjoelquinn <alistairjoelquinn@gmail.com>
2022-09-19 12:44:09 +0200

    geins refactorsing server side files to new structure

--- server/schemas/best-practise.js
+++ server/schemas/best-practise.js
@@ -1,25 +1,23 @@
 import { BsTrophy as icon } from 'react-icons/bs';
 
 export default {
-    name: 'best-practise',
-    title: 'General Best Practise',
-    type: 'document',
-    icon,
-    fields: [
-        {
-            name: 'title',
-            title: 'Title',
-            type: 'string',
-            description: 'What is the best practise being described?'
-        },
-        {
-            name: 'value',
-            title: 'Description',
-            type: 'array',
-            of: [
-                { type: 'string' }
-            ],
-            description: 'Each input field will become a separate paragraph.'
-        },
-    ]
+  name: 'best-practise',
+  title: 'General Best Practise',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      description: 'What is the best practise being described?',
+    },
+    {
+      name: 'value',
+      title: 'Description',
+      type: 'array',
+      of: [{ type: 'string' }],
+      description: 'Each input field will become a separate paragraph.',
+    },
+  ],
 };
--- server/schemas/common-words.js
+++ server/schemas/common-words.js
@@ -1,28 +1,29 @@
 import { BsBookHalf as icon } from 'react-icons/bs';
 
 export default {
-    name: 'common-words',
-    title: 'Common Words',
-    type: 'document',
-    icon,
-    fields: [
-        {
-            name: 'title',
-            title: 'Word',
-            type: 'string',
-            description: 'What is the word?'
-        },
-        {
-            name: 'value',
-            title: 'Description',
-            type: 'string',
-            description: 'A more detailed description of why it is included.'
-        },
-        {
-            name: 'category',
-            title: 'Category',
-            type: 'string',
-            description: 'Which letter should this word appear under? Always capitalise. e.g. "A" for Adidis, "#" for anything not falling under a letter category.'
-        },
-    ]
+  name: 'common-words',
+  title: 'Common Words',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'title',
+      title: 'Word',
+      type: 'string',
+      description: 'What is the word?',
+    },
+    {
+      name: 'value',
+      title: 'Description',
+      type: 'string',
+      description: 'A more detailed description of why it is included.',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      description:
+        'Which letter should this word appear under? Always capitalise. e.g. "A" for Adidis, "#" for anything not falling under a letter category.',
+    },
+  ],
 };
