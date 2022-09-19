4a96da0f3b1f9dab15495eab06e3730eece01826
alistairjoelquinn <alistairjoelquinn@gmail.com>
2022-09-19 13:02:01 +0200

    refactors dates and numbers

--- server/schemas/dates-numbers.js
+++ server/schemas/dates-numbers.js
@@ -1,25 +1,24 @@
 import { ImListNumbered as icon } from 'react-icons/im';
 
 export default {
-    name: 'dates-numbers',
-    title: 'Dates & Numbers',
-    type: 'document',
-    icon,
-    fields: [
-        {
-            name: 'title',
-            title: 'Title',
-            type: 'string',
-            description: 'What is the item?'
-        },
-        {
-            name: 'value',
-            title: 'Description',
-            type: 'array',
-            of: [
-                { type: 'string' }
-            ],
-            description: 'Each input should be a separate example clarifying why the item is included.'
-        },
-    ]
+  name: 'dates-numbers',
+  title: 'Dates & Numbers',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      description: 'What is the item?',
+    },
+    {
+      name: 'value',
+      title: 'Description',
+      type: 'array',
+      of: [{ type: 'string' }],
+      description:
+        'Each input should be a separate example clarifying why the item is included.',
+    },
+  ],
 };
